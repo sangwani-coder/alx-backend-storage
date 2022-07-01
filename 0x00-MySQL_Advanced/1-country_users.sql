0102966694ca7393d68d381271b2b3e8ceb86039
unknown <mightypz2@gmail.com>
2022-07-01 19:59:05 +0200

    remove hard coded ENUM default value

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS users(
 	id INT NOT NULL AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL,
 	name VARCHAR(255) NOT NULL,
-	country ENUM('US','CO','TN') DEFAULT 'US',
+	country ENUM('US','CO','TN') DEFAULT 0,
 	UNIQUE (email),
 	PRIMARY KEY (id)
 	);
