40643c6eaa62b22076cadf836072efbb818c8dd7
zyambo <mightypz2@gmail.com>
2022-07-05 14:21:52 +0000

    [Fix] prevent nodule being executed when imported

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,10 +1,11 @@
 #!usr/bin/env python3
 """ pymongo list"""
-from pymongo import MongoClient
 
+if __name__ == "__main__":
+    from pymongo import MongoClient
 
-def list_all(mongo_collection):
-    """ finds all documents form pymongo collection"""
-    if not mongo_collection:
-        return []
-    return list(mongo_collection.find())
+    def list_all(mongo_collection):
+        """ finds all documents form pymongo collection"""
+        if not mongo_collection:
+            return []
+        return list(mongo_collection.find())
