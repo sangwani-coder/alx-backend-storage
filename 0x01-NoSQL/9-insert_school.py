c4aac47f325fc5a8d680c231d63c2f1d2a8da53e
zyambo <mightypz2@gmail.com>
2022-07-05 13:38:03 +0200

    complete task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin//env python3
+""" Pymongo collection object"""
+from pymongo import MongoClient
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ inserts a new document into a collection"""
+    if len(kwargs) == 0:
+        return None
+    return mongo_collection.insert(kwargs)
+
