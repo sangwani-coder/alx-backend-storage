831017605a6f41da9186559076b1fed9a16d0813
zyambo <mightypz2@gmail.com>
2022-07-05 14:39:55 +0000

    Fix typo in HashBang

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,10 +1,12 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 """ pymongo list"""
 import pymongo
 
 
 def list_all(mongo_collection):
-    """ finds all documents form pymongo collection"""
+    """
+        finds all documents form pymongo collection
+    """
     if not mongo_collection:
         return []
     return list(mongo_collection.find())
