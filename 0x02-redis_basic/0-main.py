ae9f6adcab324ef66b09891c7b0331c6b1672f67
zyambo <mightypz@gmail.com>
2022-07-06 21:24:01 +0000

    Fix typo in constructor

--- 0x02-redis_basic/0-main.py
+++ 0x02-redis_basic/0-main.py
@@ -8,7 +8,7 @@ Cache = __import__('exercise').Cache
 
 cache = Cache()
 
-data = b"hello"
+data = 800
 key = cache.store(data)
 print(key)
 
--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,9 +8,9 @@ from typing import Union, Type
 class Cache():
     """ stores an instance of the redis client"""
     def __init__(self) -> None:
-        """ instatiate the Cache object"""
+        """ Constructor"""
         self._redis: Type = redis.Redis()
-        self._redis.flushdb
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """" generate a random key using uuid"""
