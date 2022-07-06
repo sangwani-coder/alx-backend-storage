3820188c6d8c5e171a25f0534c590bb20c3ce116
zyambo <mightypz@gmail.com>
2022-07-06 22:01:10 +0000

    Complete task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ defines a class Cache"""
 import redis
 import uuid
-from typing import Union, Type
+from typing import Union, Type, Callable
 
 
 class Cache():
@@ -17,3 +17,27 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self
+            key: str,
+            fn: Optional[Callabe] = None) -> Union[str, byrtes, int, float]:
+        """ extract informtion saved in redis"""
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """ utomatically parametrize Cache.get
+            with the correct conversion function.
+        """
+        value = self._redis.get(key)
+        return value.decode('UTF-8')
+
+    def get_int(self, key: str) -> int:
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode('UTF-8'))
+        except Exception:
+            value = 0
+        return value
