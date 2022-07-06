2b97881a04785f20c140cb626bff978e622ed9a5
zyambo <mightypz@gmail.com>
2022-07-06 21:18:58 +0000

    [FIX] change annotation type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,17 +1,18 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 """ defines a class Cache"""
 import redis
 import uuid
+from typing import Union, Type
 
 
 class Cache():
     """ stores an instance of the redis client"""
-    def __init__(self):
+    def __init__(self) -> None:
         """ instatiate the Cache object"""
-        self._redis = redis.Redis()
+        self._redis: Type = redis.Redis()
         self._redis.flushdb
 
-    def store(self, data: any) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """" generate a random key using uuid"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
