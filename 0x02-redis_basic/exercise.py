e54d6c3c96686513c69e1a9611dad324380dd317
zyambo <mightypz@gmail.com>
2022-07-06 21:47:41 +0000

    reset to old set function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,13 +7,13 @@ from typing import Union, Type
 
 class Cache():
     """ stores an instance of the redis client"""
-    def __init__(self) -> None:
+    def __init__(self):
         """ Constructor"""
-        self._redis: Type = redis.Redis()
+        self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """" generate a random key using uuid"""
         key = str(uuid.uuid4())
-        self._redis.mset({key: data})
+        self._redis.set(key, data)
         return key
